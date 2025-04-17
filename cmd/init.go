2cbb4da9ea6f9f0e05777a1a68b2d90c3e63c104
Arnav Surve <arnav@surve.dev>
2025-04-17 16:37:12 -0700

    grace init with target dir or scatter in current. templates to be implemented

--- cmd/init.go
+++ cmd/init.go
@@ -1,19 +1,79 @@
 package cmd
 
 import (
+	"embed"
 	"fmt"
+	"os"
+	"path/filepath"
+	"text/template"
 
 	"github.com/spf13/cobra"
 )
+
+//go:embed templates/*
+var tplFS embed.FS
 
 // init: scaffold a new job
 var InitCmd = &cobra.Command{
 	Use:   "init [job-name]",
 	Short: "Scaffold a new Grace job",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		jobName := args[0]
+		var (
+			targetDir string
+			jobName   string
+		)
+
+		// Determine where to write files
+		// targetDir is where files go, jobName is for templating
+		if len(args) == 1 {
+			targetDir = args[0]
+			jobName = args[0]
+		} else {
+			targetDir = "."        // scatter in cwd
+			cwd, err := os.Getwd() // use base name for templating
+			cobra.CheckErr(err)
+			jobName = filepath.Base(cwd)
+		}
+
+		// If we are making a new subdirectory, ensure it doesn't already exist
+		if targetDir != "." {
+			if _, err := os.Stat(targetDir); err == nil {
+				cobra.CheckErr(fmt.Errorf("directory %q already exists", targetDir))
+			}
+
+			err := os.MkdirAll(targetDir, 0755)
+			cobra.CheckErr(err)
+		}
+
 		fmt.Printf("↪ scaffolding new job %q ...\n", jobName)
-		// TODO: create directory, copy template files, etc
+
+		// Copy each template to destination with template data
+		data := map[string]string{"JobName": jobName}
+
+		files := map[string]string{
+			"templates/hello.grc.tpl": "hello.grc",
+			"templates/grace.yml.tpl": "grace.yml",
+		}
+
+		for tplPath, outName := range files {
+			outPath := filepath.Join(targetDir, outName)
+			writeTpl(tplPath, outPath, data)
+		}
+
+		fmt.Printf("✓ job %q initialized!\n", jobName)
 	},
+}
+
+// writeTpl loads tplName from tplFS, executes it with data, and writes to outPath
+func writeTpl(tplName, outPath string, data any) {
+	t, err := template.ParseFS(tplFS, tplName)
+	cobra.CheckErr(err)
+
+	f, err := os.Create(outPath)
+	cobra.CheckErr(err)
+	defer f.Close()
+
+	err = t.Execute(f, data)
+	cobra.CheckErr(err)
 }
